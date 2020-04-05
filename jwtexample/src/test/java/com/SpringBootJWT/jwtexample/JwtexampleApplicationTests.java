3f2f343f746d0ce9bd7917cda08958a9cfae396b
LaxmiSundari <45286935+LaxmiSundari@users.noreply.github.com>
2020-04-05 19:44:52 +0530

    Initial structure of the project uploaded

---
+++ jwtexample/src/test/java/com/SpringBootJWT/jwtexample/JwtexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootJWT.jwtexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
